191d3ffc13a554d9cc97366075d11742841ab0b0
matthewparris@live.ca <Matthew>
2020-01-14 23:26:37 -0500

    Updates

---
+++ precache-manifest.7675b102993a4872781a9ca25380f813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0640038d83f5153265fa2a5c29b8e54a",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc7152df501abf98167a",
+    "url": "/react-monsters-rolodex/static/css/main.1948b580.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc7152df501abf98167a",
+    "url": "/react-monsters-rolodex/static/js/main.b0d1426f.chunk.js"
+  },
+  {
+    "revision": "2a54011d4b12cab4d01e",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.753a6df2.js"
+  }
+]);
